da1bdeb9bb1b202dea5556ca1fe9b5624cb454d7
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-11-13 08:06:39 -0300

    feat: react query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 import { Toaster } from 'sonner'
 
+import { ReactQueryProvider } from '@/components/provider/react-query-provider'
 import { ThemeProvider } from '@/components/theme-provider'
 import { cn } from '@/lib/utils'
 
@@ -32,14 +33,16 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <ReactQueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ReactQueryProvider>
         <Toaster richColors />
       </body>
     </html>
