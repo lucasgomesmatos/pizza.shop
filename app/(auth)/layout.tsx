235aed99c7d93f879834c59c59aad3b9405d2423
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-11-15 23:52:36 -0300

    feat: ky client api

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export default function AuthLayout({
 }>) {
   return (
     <section className="grid min-h-screen grid-cols-2">
-      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+      <section className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
         <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="size-5" />
           <span className="font-semibold">pizza.shop</span>
@@ -15,7 +15,7 @@ export default function AuthLayout({
         <footer className="text-sm">
           Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
         </footer>
-      </div>
+      </section>
       <div className="relative flex flex-col items-center justify-center">
         {children}
       </div>
