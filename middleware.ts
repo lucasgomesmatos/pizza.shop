9281d71a9fdda6801250c9ac73aa5893edfe0133
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-11-20 00:03:07 -0300

    feat: middleware redirect

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,18 @@
-import { NextRequest, NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
 const publicRoutes = ['/sign-up', '/sign-in']
 
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
+  const authenticated = request.cookies.get('auth')
+
+  if (!authenticated && !publicRoutes.includes(request.nextUrl.pathname)) {
+    const url = new URL('/sign-in', request.url)
+    return NextResponse.redirect(url)
+  }
+
   return NextResponse.next()
 }
-
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$|.*\\.jpg$).*)'],
 }
