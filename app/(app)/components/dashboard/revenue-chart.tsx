1002f3bfc3a2624c975258a588e481b0f68187b7
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-12-30 20:25:49 -0300

    feat: add date range metrics

--- app/(app)/components/dashboard/revenue-chart.tsx
+++ app/(app)/components/dashboard/revenue-chart.tsx
@@ -1,5 +1,8 @@
 'use client'
 import { useQuery } from '@tanstack/react-query'
+import { subDays } from 'date-fns'
+import { useMemo, useState } from 'react'
+import { DateRange } from 'react-day-picker'
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts'
 import colors from 'tailwindcss/colors'
 
@@ -16,7 +19,10 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from '@/components/ui/chart'
+import { Label } from '@/components/ui/label'
 import { getDailyRevenueInPeriod } from '@/http/get-daily-revenue-in-period'
+
+import { DateRangePicker } from './date-range-picker'
 
 const chartConfig = {
   date: {
@@ -30,24 +36,47 @@ const chartConfig = {
 } satisfies ChartConfig
 
 export const RevenueChart = () => {
-  const { data: dailyRevenueInPeriod } = useQuery({
-    queryKey: ['metrics', 'daily-revenue-in-period'],
-    queryFn: getDailyRevenueInPeriod,
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: subDays(new Date(), 7),
+    to: new Date(),
   })
+
+  const { data: dailyRevenueInPeriod } = useQuery({
+    queryKey: ['metrics', 'daily-revenue-in-period', dateRange],
+    queryFn: () =>
+      getDailyRevenueInPeriod({
+        from: dateRange?.from,
+        to: dateRange?.to,
+      }),
+  })
+
+  const chartData = useMemo(() => {
+    return dailyRevenueInPeriod?.map((chartItem) => {
+      return {
+        date: chartItem.date,
+        receipt: chartItem.receipt / 100,
+      }
+    })
+  }, [dailyRevenueInPeriod])
 
   return (
     <Card className="col-span-6">
-      <CardHeader>
+      <CardHeader className="flex flex-row items-center justify-between pb-8">
         <div className="space-y-1">
           <CardTitle>Receita no período</CardTitle>
           <CardDescription>Receita diária no período</CardDescription>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Label>Período</Label>
+          <DateRangePicker date={dateRange} onDateChange={setDateRange} />
         </div>
       </CardHeader>
       <CardContent>
         {dailyRevenueInPeriod && (
           <ChartContainer config={chartConfig} className="h-[250px] w-full">
             <LineChart
-              data={dailyRevenueInPeriod}
+              data={chartData}
               margin={{
                 top: 20,
                 left: 14,
--- http/get-daily-revenue-in-period.ts
+++ http/get-daily-revenue-in-period.ts
@@ -1,13 +1,26 @@
 import { api } from './api-client'
 
+export interface GetDailyRevenueInPeriodQuery {
+  from?: Date
+  to?: Date
+}
+
 type GetDailyRevenueInPeriodResponse = {
   date: string
   receipt: number
 }[]
 
-export async function getDailyRevenueInPeriod() {
+export async function getDailyRevenueInPeriod({
+  from,
+  to,
+}: GetDailyRevenueInPeriodQuery) {
   const response = await api
-    .get('metrics/daily-receipt-in-period')
+    .get('metrics/daily-receipt-in-period', {
+      searchParams: new URLSearchParams({
+        from: from!.toString(),
+        to: to!.toString(),
+      }),
+    })
     .json<GetDailyRevenueInPeriodResponse>()
 
   return response
